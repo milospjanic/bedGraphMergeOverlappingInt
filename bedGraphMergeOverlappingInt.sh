46cd21b9801f1f46a31f5e85e8c1b9aa46c2fed8
Milos Pjanic <pjanic.milos@gmail.com>
2017-03-14 16:50:37 -0700

    Update bedGraphMergeOverlappingInt.sh

--- bedGraphMergeOverlappingInt.sh
+++ bedGraphMergeOverlappingInt.sh
@@ -6,7 +6,7 @@ BEDGRAPH=$(pwd)/$1
 echo Proccesing file:
 echo $BEDGRAPH
 
-#merge intervals for each chromose separately, needs perl installed
+#merge intervals for each chromosome separately, needs perl installed
 
 for chr in {1..22} X Y 
 
@@ -14,7 +14,7 @@ do
 
 export chr
 
-(cut -f2 %BEDGRAPH; cut -f3 %BEDGRAPH) | sort -nu | perl -ne 'BEGIN{$i=0} chomp; push @F, $_; if($i++){print "chr$ENV{chr}\t$F[$i-2]\t$F[$i-1]\n"}' > b.bed
+(cut -f2 $BEDGRAPH; cut -f3 $BEDGRAPH) | sort -nu | perl -ne 'BEGIN{$i=0} chomp; push @F, $_; if($i++){print "chr$ENV{chr}\t$F[$i-2]\t$F[$i-1]\n"}' > b.bed
 
 bedtools intersect -a $BEDGRAPH -b b.bed | sort -k2n -k3n -k4nr | perl -lane 'print unless $h{$F[0,1,2]}++' > $BEDGRAPH.$chr
 
