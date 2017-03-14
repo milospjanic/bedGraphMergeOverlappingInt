29ef73074c0d5681773075ac84de9ffa9b596135
Milos Pjanic <pjanic.milos@gmail.com>
2017-03-14 15:43:50 -0700

    Update bedGraphMergeOverlappingInt.sh

--- bedGraphMergeOverlappingInt.sh
+++ bedGraphMergeOverlappingInt.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-for chr in {1..22}
+###bedGraphMergeOverlappingInt
+
+#merge intervals for each chromose separately, needs perl installed
+
+for chr in {1..22} X Y 
 
 do
 
@@ -12,6 +16,16 @@ bedtools intersect -a out.bedGraph.hg19.sort -b b.bed | sort -k2n -k3n -k4nr | p
 
 done
 
-cat out.bedGraph.hg19.sort.chr* >out.bedGraph.hg19.sort.merge
+#concatenate individual chromosome files
+
+for chr in {1..22} X Y 
+
+do
+
+cat out.bedGraph.hg19.sort.$chr >> out.bedGraph.hg19.sort.merge
+
+done
+
+#sort merged file
 
 sort -k1,1 -k2,2n out.bedGraph.hg19.sort.merge > out.bedGraph.hg19.sort.merge.sort
